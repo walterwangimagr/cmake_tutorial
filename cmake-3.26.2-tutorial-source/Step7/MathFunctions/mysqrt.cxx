206290920dbada449322ac920129fc046cf27030
walter <walter.wang@imagr.co>
2023-04-03 15:29:56 +1200

    check the target platform contains some required functions like log or exp

--- cmake-3.26.2-tutorial-source/Step7/MathFunctions/mysqrt.cxx
+++ cmake-3.26.2-tutorial-source/Step7/MathFunctions/mysqrt.cxx
@@ -1,6 +1,7 @@
 #include <iostream>
 
 // TODO 4: include cmath
+#include <cmath>
 #include "MathFunctions.h"
 
 // a hack square root calculation using simple operations
@@ -17,8 +18,12 @@ double mysqrt(double x)
   // else, use the existing logic.
 
   // Hint: Don't forget the #endif before returning the result!
-
+#if defined(HAVE_LOG) && defined(HAVE_EXP)
+  double result = std::exp(std::log(x) * 0.5);
+  std::cout << "Computing sqrt of " << x << " to be " << result << " using log and exp" << std::endl;
+#else
   double result = x;
+#endif
 
   // do ten iterations
   for (int i = 0; i < 10; ++i) {
